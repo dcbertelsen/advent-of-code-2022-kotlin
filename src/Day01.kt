b582eaf6b2dec0cb6265f6ac55a4ecb8a9584171
dcbertelsen <douglas.bertelsen@gmail.com>
2022-12-01 21:14:52 -0600

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map{
+            it.split("\n")
+        }
+            .map {
+                it.sumOf { v -> v.toInt() }
+            }
+            .maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map{
+                it.split("\n")
+            }
+            .map {
+                it.sumOf { v -> v.toInt() }
+            }
+            .sorted()
+            .takeLast(3)
+            .sum()
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
+    val input = File("./src/main/resources/twentytwo/Day01a.txt").readText().split("\n\n")
     println(part1(input))
     println(part2(input))
 }
